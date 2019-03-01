9c188d01a42b1bb0237cc8386852716a6c161dae
Eric Daoud <daoudattoyan.eric@gmail.com>
2019-03-01 18:53:51 +0100

    favicon in publishconf

--- publishconf.py
+++ publishconf.py
@@ -12,6 +12,7 @@ from pelicanconf import *
 
 SITEURL = 'https://ericdaat.github.io'
 RELATIVE_URLS = False
+FAVICON = SITEURL + 'favicon.ico'
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
 CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
