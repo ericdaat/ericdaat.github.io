125e915be5364c055bfd78f8fed399daa035a7f9
Eric Daoud <daoudattoyan.eric@gmail.com>
2023-12-30 16:38:27 +0100

    drafted article

---
+++ src/@lekoarts/gatsby-theme-jodie-core/templates/project-query.tsx
@@ -0,0 +1,40 @@
+import { graphql } from "gatsby"
+import ProjectComponent, { Head } from "../../gatsby-theme-jodie/components/project"
+
+export default ProjectComponent
+
+export { Head }
+
+export const query = graphql`
+  query ($slug: String!, $formatString: String!, $relativeDirectory: String!) {
+    project(slug: { eq: $slug }) {
+      excerpt
+      color
+      date(formatString: $formatString)
+      slug
+      title
+      shortTitle
+      category
+      cover {
+        childImageSharp {
+          resize(width: 1200, quality: 85) {
+            src
+          }
+        }
+      }
+    }
+    images: allFile(
+      filter: {
+        relativeDirectory: { eq: $relativeDirectory }
+        extension: { regex: "/(jpg)|(png)|(tif)|(tiff)|(webp)|(jpeg)/" }
+      }
+    ) {
+      nodes {
+        name
+        childImageSharp {
+          gatsbyImageData(width: 1600, quality: 90)
+        }
+      }
+    }
+  }
+`
